4bca15bad376fb5787371380e79a2392571a47de
Matthew McEntire <mmcentire@healthequity.com>
2020-12-07 13:11:55 -0700

    Completed Day one challenge

---
+++ Day1/index.js
@@ -0,0 +1,24 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') {
+        return;
+    }
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`kbd[data-key="${e.keyCode}"]`);
+    if (!audio || audio == null) {
+        return;
+    }
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  console.log(keys);
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+  window.addEventListener("keyup", removeTransition);
