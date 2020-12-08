8789f4bf11293e02f205006db0b407a02d1be8af
Matthew McEntire <mmcentire@healthequity.com>
2020-12-08 16:38:44 -0700

    completed day 3.

---
+++ Day3/index.js
@@ -0,0 +1,14 @@
+function handleUpdate(){
+    const suffix = this.dataset.sizing || "";
+    console.log(this.name);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+function onControlsChange(){
+    var inputs = document.querySelectorAll("#controls input");
+    
+    inputs.forEach(input => input.addEventListener("change", handleUpdate));
+    inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+}
+
+onControlsChange();
